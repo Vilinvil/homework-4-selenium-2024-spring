3fa4e67ac77992f0e03a51c98e0540bebae7e364
vilinvil <ivn15072002@gmail.com>
2024-05-12 19:01:24 +0300

    change locator on BasePage to more constant

--- hw/code/ui/locators/basic_locators.py
+++ hw/code/ui/locators/basic_locators.py
@@ -2,4 +2,4 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    BUTTON_CABINET_LOCATOR = (By.CLASS_NAME, 'ButtonCabinet_primary__LCfol')
+    BUTTON_CABINET_LOCATOR = (By.CSS_SELECTOR, '[href="/hq?ref=main_landing"]')
