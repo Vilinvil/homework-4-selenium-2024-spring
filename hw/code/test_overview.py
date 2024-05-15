fd7321b53566f9619595ecf80b54fce902897c73
vilinvil <ivn15072002@gmail.com>
2024-05-15 11:20:05 +0300

    add test_open_page_by_click in TestNewUserOverview

--- hw/code/test_overview.py
+++ hw/code/test_overview.py
@@ -2,6 +2,9 @@ import pytest
 
 from cases import LoggedNewUserCase
 from ui.pages.overview_page import OverviewNewUserPage
+from ui.locators.overview_locators import OverviewNewUserPageLocators
+from ui.locators.campaigns_locators import CampaignsPageSharedLocators
+from ui.locators.training_locators import TrainingPageSharedLocators
 
 
 class TestNewUserOverview(LoggedNewUserCase):
@@ -11,3 +14,18 @@ class TestNewUserOverview(LoggedNewUserCase):
 
     def test_display(self):
         self.overview_new_user_page.check_start_actions()
+
+    @pytest.mark.parametrize('button_locator, expected_locator',
+                             [
+                                 pytest.param(
+                                     OverviewNewUserPageLocators.BUTTON_CREATE_CAMPAIGN,
+                                     CampaignsPageSharedLocators.SIGN_NEW_AD_CREATE
+                                 ),
+                                 pytest.param(
+                                     OverviewNewUserPageLocators.BUTTON_START_TRAINING,
+                                     TrainingPageSharedLocators.MODAL_PAGE,
+                                 )
+                             ], )
+    def test_open_page_by_click(self, button_locator, expected_locator):
+        self.overview_new_user_page.click(button_locator)
+        assert self.overview_new_user_page.find(expected_locator).is_displayed()
--- hw/code/ui/locators/overview_locators.py
+++ hw/code/ui/locators/overview_locators.py
@@ -3,5 +3,10 @@ from selenium.webdriver.common.by import By
 
 class OverviewNewUserPageLocators:
     START_ACTIONS_WRAPPER = (By.XPATH, '//*[starts-with(@class, "StartActions_wrapper")]')
-    START_ACTION = (By.XPATH,'//*[starts-with(@class, "StartAction_wrapper")]')
+    START_ACTION = (By.XPATH,'.//*[starts-with(@class, "StartAction_wrapper")]')
     BUTTON_IN_START_ACTION_WRAPPER = (By.XPATH, './/button')
+
+    BUTTON_CREATE_CAMPAIGN =\
+        (By.XPATH, '//*[starts-with(@class, "StartAction_wrapper")]//button//*[contains(text(),"Создать вручную")]')
+    BUTTON_START_TRAINING = \
+        (By.XPATH, '//*[starts-with(@class, "StartAction_wrapper")]//button//*[contains(text(),"Пройти обучение")]')
