adfac2eeb4a35289b58aa93ca03175d9cfdf441e
Alexander Tarba <sachatarba@rambler.ru>
2024-05-23 03:18:16 +0300

    переписать на декораторы

--- hw/code/ui/pages/lead_forms_page.py
+++ hw/code/ui/pages/lead_forms_page.py
@@ -8,73 +8,118 @@ class LeadFormsPage(PageWithModalView, PageWithRedirectWindow):
     locators = LeadFormsPageLocators()
 
 
-    def get_create_lead_form_button(self):
+    def add_click(button_getter):
+        """Adds click method to button"""
+        def decorator(self):
+            def click():
+                """Clicks button"""
+                self.click(button_getter(self))
+
+            button_getter(self).click = click
+            return button_getter(self)
+        return decorator
+    
+
+    def add_enter(input_field_getter):
+        """Adds enter method to input_field"""
+        def decorator(self):
+            def enter(text):
+                """Enters text in input field"""
+                input_field_getter(self).clear()
+                input_field_getter(self).send_keys(text)
+
+            input_field_getter(self).enter = enter
+            return input_field_getter(self)
+        return decorator
+    
+
+    @property
+    @add_click
+    def CREATE_LEAD_FORM_BUTTON(self):
         return self.find(self.locators.CREATE_FORM_BUTTON,
                                        until_EC=EC.visibility_of_element_located)
 
 
-    # def get_search_status_dropmenu(self):
-        # return self.find(self.locators.DROP_MENU)
-    
-
-    def get_search_leadform_input_field(self):
+    @property
+    @add_enter
+    def SEARCH_LEAD_FORM_INPUT(self):
         return self.find(self.locators.SEARCH_INPUT,
                                        until_EC=EC.visibility_of_element_located)
     
 
-    def get_lead_forms_tab(self):
+    @property
+    @add_click
+    def LEAD_FORM_TAB(self):
         return self.find(self.locators.LEADFORMS_TAB,
                                        until_EC=EC.visibility_of_element_located)
     
-
-    def get_yclients_tab(self):
+    
+    @property
+    @add_click
+    def YCLIENTS_TAB(self):
         return self.find(self.locators.YCLIENTS_TAB,
                                        until_EC=EC.visibility_of_element_located)
-    
-    
-    def get_surveys_tab(self):
+        
+
+    @property
+    @add_click
+    def SURVEYS_TAB(self):
         return self.find(self.locators.SURVEYS_TAB,
                                        until_EC=EC.visibility_of_element_located)
+
+
+    @property
+    @add_enter
+    def LEAD_FORM_NAME_INPUT(self):
+        return self.find(self.locators.INPUT_FORM_NAME)
     
 
-    def click_create_lead_form_button(self):
-        self.click(self.get_create_lead_form_button())
-
-
-    def get_name_lead_form_input_field(self):
-        return self.find(self.locators.INPUT_FORM_NAME)
-
-
-    def get_name_company_input_field(self):
+    @property
+    @add_enter
+    def COMPANY_NAME_INPUT(self):
         return self.find(self.locators.INPUT_COMPANY_NAME)
 
 
-    def get_header_input_field(self):
+    @property
+    @add_enter
+    def HEADER_INPUT_FIELD(self):
         return self.find(self.locators.INPUT_HEADER)
     
 
-    def get_surveys_info_input_field(self):
+    @property
+    @add_enter
+    def SURVEYS_INFO_INPUT(self):
         return self.find(self.locators.INPUT_SURVEYS_INFO)
     
 
-    def get_choose_image_button(self):
+    @property
+    @add_click
+    def CHOOSE_IMAGE_BUTTON(self):
         return self.find(self.locators.CHOOSE_IMAGE_BUTTON)
     
 
-    def get_cancel_button(self):
+    @property
+    @add_click
+    def CANCEL_BUTTON(self):
         return self.find(self.locators.CANCEL_BUTTON)
     
 
-    def get_continue_button(self):
+    @property
+    @add_click
+    def CONTINUE_BUTTON(self):
         return self.find(self.locators.CONTINUE_BUTTON)
     
 
-    def get_compact_button(self):
+    @property
+    @add_click
+    def COMPACT_BUTTON(self):
         return self.find(self.locators.COMPACT_BUTTON)
     
 
-    def get_more_text_button(self):
+    @property
+    @add_click
+    def MORE_TEXT_BUTTON(self):
         return self.find(self.locators.MORE_TEXT_BUTTON)
-    
+     
     
     
